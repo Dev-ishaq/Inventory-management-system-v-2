dfd722a89e7727788e84ab84ba7dc87609276235
Dev-ishaaq <ishaqak101@gmail.com>
2023-09-16 14:33:52 -0700

    Added something

--- admin/manage_staff.php
+++ admin/manage_staff.php
@@ -46,6 +46,10 @@
                     </div>
                 </form>
             </div>
+
+
+
+            
         </div>
         <table class="table table-success table-striped table-bordered mt-3">
             <thead>
