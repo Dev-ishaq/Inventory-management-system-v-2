e3ab8adf00db688c5980ec1bf01fff9d480b1fca
Dev-ishaaq <ishaqak101@gmail.com>
2023-09-27 22:36:00 -0700

    Added something

--- admin/manage_staff.php
+++ admin/manage_staff.php
@@ -39,7 +39,7 @@
         <a href="../admin/admin_dashboard.php" class="text-left btn btn-primary mt-5"><i class="fa fa-backward"></i> &nbsp;&nbsp;BACK</a>
         <a href="../admin/reg2.php" class="text-right btn btn-success mt-5"><i class="fa fa-users"></i> &nbsp;&nbsp;ADD STAFF</a>
         
-        <table class="table table-success table-striped table-bordered mt-3">
+        <table class="table table-success table-striped table-bordered mt-3" id="table">
             <thead>
               <tr>
                 <th>S/NO</th>
@@ -87,7 +87,7 @@
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
    <script>
       $(document).ready(function(){
-        $('.table').dataTable();
+        let table = new DataTable('#table');
         
       });
    </script>
