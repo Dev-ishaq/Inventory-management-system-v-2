13c21d987b79c2c41e34b34de64da0a89fc64da8
Dev-ishaaq <ishaqak101@gmail.com>
2023-09-27 22:27:46 -0700

    Added something

--- admin/manage_staff.php
+++ admin/manage_staff.php
@@ -83,8 +83,8 @@
    </div>
    
    <script src="../css/JQ.js"></script>
-   <script src="../css/DataTables-1.13.6/js/jquery.dataTables.min.js"></script>
-   <script src="../css/bootstrap/js/bootstrap.min.js"></script>
+   <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
    <script>
       $(document).ready(function(){
         $('.table').dataTable();
